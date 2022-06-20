72ca20dd6358ae464811753e85f11ec3be4294d1
dan_hates_the_future <97214702+dangalansky@users.noreply.github.com>
2022-06-20 12:09:39 -0600

    Add files via upload
    Update.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ x_score = 0
 o_score = 0
 game_over = False
 
+
 def score_board():
     global x_score
     global o_score
@@ -23,7 +24,7 @@ def print_board():
 def check_for_draw():
     global game_over
     global board
-    if not '-' in board:
+    if '-' not in board:
         print('Game over! It\'s A Draw!')
         again = input('Would you like to play again? Y or N: ')
         if again.lower() == 'y':
@@ -35,32 +36,31 @@ def check_for_draw():
             score_board()
 
 
-
 def check_for_win():
     global game_over, board, x_score, o_score
     if board[0] == board[1] == board[2] == 'X' or board[3] == board[4] == board[5] == 'X' or board[0] == board[4] == \
             board[8] == 'X' or board[2] == board[4] == board[6] == 'X' or board[0] == board[3] == board[6] == 'X' or \
             board[1] == board[4] == board[7] == 'X' or board[2] == board[5] == board[8] == 'X':
-        x_score += 1
         print('\nX WINS!')
+        x_score += 1
         again = input('Would you like to play again? Y or N: ')
         if again.lower() == 'y':
             board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
             game_on()
-        else:
+        if again.lower() == 'n':
             game_over = True
             print('Final Score:')
             score_board()
     if board[0] == board[1] == board[2] == 'O' or board[3] == board[4] == board[5] == 'O' or board[0] == board[4] == \
             board[8] == 'O' or board[2] == board[4] == board[6] == 'O' or board[0] == board[3] == board[6] == 'O' or \
             board[1] == board[4] == board[7] == 'O' or board[2] == board[5] == board[8] == 'O':
-        o_score += 1
         print('\nO WINS!')
+        o_score += 1
         again = input('Would you like to play again? Y or N: ')
         if again.lower() == 'y':
             board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
             game_on()
-        else:
+        if again.lower() == 'n':
             game_over = True
             print('Final Score:')
             score_board()
@@ -106,17 +106,17 @@ def o_turn():
 
 def game_on():
     score_board()
-    number = randint(0,1)
+    number = randint(0, 1)
     if number == 1:
         print('X Goes First')
-    else:
+        while not game_over:
+            x_turn()
+            o_turn()
+    if number == 0:
         print('0 Goes First')
-    while not game_over:
-        if number == 1:
-            x_turn()
-            o_turn()
-        else:
+        while not game_over:
             o_turn()
             x_turn()
+
 
 game_on()
